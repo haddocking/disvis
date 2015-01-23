d2aa3b7b7611c8ec46312d137b9f9a30a67d1734
Alexandre Bonvin <alexandre.bonvin@gmail.com>
2015-01-23 17:46:54 +0100

    Added missing bits for installation following referee comments

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
+import numpy
+
 ext_modules = [Extension("disvis/libdisvis",
         ["src/libdisvis.pyx"])]
 
@@ -20,4 +22,5 @@ setup(name="disvis",
       package_data = package_data,
       scripts=scripts,
       requires=['numpy', 'cython'],
+      include_dirs=[numpy.get_include()],
     )
