2009e64d17dcb48546eb52b3410880deac0aa2b1
Gydo <g.c.p.vanzundert@uu.nl>
2015-02-26 15:36:06 +0100

    Added random_rotation function.

--- disvis/rotations.py
+++ disvis/rotations.py
@@ -55,7 +55,7 @@ def quat_to_rotmat(quaternions, invert=False):
 
     return rotmat
 
-def random_orientation():
+def random_rotation():
 
 
     while True:
@@ -76,6 +76,18 @@ def random_orientation():
     q1 = e2
     q2 = e3 * sqrt( (1 - s1)/s2 )
     q3 = e4 * sqrt( (1 - s1)/s2 )
+
+    a11 = q0*q0 + q1*q1 - q2*q2 - q3*q3
+    a12 = 2 * (q1 * q2 - q0 * q3)
+    a13 = 2 * (q1 * q3 + q0 * q2)
+    a21 = 2 * (q1 * q2 + q0 * q3)
+    a22 = q0*q0 - q1*q1 + q2*q2 - q3*q3
+    a23 = 2 * (q2 * q3 - q0 * q1)
+    a31 = 2 * (q1 * q3 - q0 * q2)
+    a32 = 2 * (q2 * q3 + q0 * q1)
+    a33 = q0*q0 - q1*q1 - q2*q2 + q3*q3
+
+    return [[a11, a12, a13], [a21, a22, a23], [a31, a32, a33]]
 
 
 def proportional_orientations(angle):
