[Update] Fixed rotation bug. GPU interaction_matrix is now working.

disvis/disvis.py
@@ -536,7 +536,7 @@ class DisVis(object):
 
     def _cl_get_restraints_center(self, rotmat):
         k = self._cl_kernels
-        k.rotate_points(self._cl_lrestraints, rotmat, self._cl_rot_lrestraints)
+        k.rotate_points3d(self.queue, self._cl_lrestraints, rotmat, self._cl_rot_lrestraints)
         k.subtract(self._cl_rrestraints, self._cl_rot_lrestraints,
                 self._cl_restraints_center)
         self.queue.finish()
@@ -577,7 +577,8 @@ class DisVis(object):
         self.queue.finish()
 
     def _cl_get_interaction_matrix(self, rotmat, weight):
-        self._cl_kernels.rotate_points(self._cl_lselect, rotmat, self._cl_rot_lselect)
+        self._cl_kernels.rotate_points3d(self.queue, self._cl_lselect, rotmat,
+                self._cl_rot_lselect)
         for nconsistent in np.arange(self.interaction_restraints_cutoff,
                 self._nrestraints + 1, dtype=np.int32):
             self._cl_interaction_hist.fill(self._CL_ZERO)

disvis/tests/test_cpu_vs_gpu.py
@@ -116,15 +116,12 @@ class TestCPUvsGPU(TestCase):
             test = np.all(self.dv._occ_grid[i] == self.gdv._cl_occ_grid[i].get())
             self.assertTrue(test)
 
-        #self.dv._get_interaction_matrix(c_rotmat, weight)
-        #self.gdv._cl_get_interaction_matrix(g_rotmat, weight)
-        #c_im = self.dv._interaction_matrix[2]
-        #g_im = self.gdv._cl_interaction_matrix[2]
-        #for i in range(c_im.shape[1]):
-        #    print c_im[i, :]
-        #    print g_im[i, :]
-        #test = np.all(c_im == g_im.get())
-        #self.assertTrue(test)
+        self.dv._get_interaction_matrix(c_rotmat, weight)
+        self.gdv._cl_get_interaction_matrix(g_rotmat, weight)
+        c_im = self.dv._interaction_matrix[2]
+        g_im = self.gdv._cl_interaction_matrix[2]
+        test = np.all(c_im == g_im.get())
+        self.assertTrue(test)
         
 
         c_rotmat = self.dv.rotations[1]