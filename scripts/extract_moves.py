f3b2d79df15e2e2e90e08ee308323355d26a3397
Gydo <gvanzundert51@gmail.com>
2016-10-09 19:04:02 -0400

    [New] Added scripts to extract translations and rotations of consistent complexes, and to generate all the PDB files.

---
+++ scripts/extract_moves.py
@@ -0,0 +1,74 @@
+"""Extract ligand translations and rotations consistent with a specified number
+of restraints."""
+
+from __future__ import division
+
+from argparse import ArgumentParser
+from os.path import abspath, getsize, join, splitext
+from glob import glob
+from sys import exit
+import operator
+
+import numpy as np
+
+from disvis.volume import Volume
+from disvis.rotations import proportional_orientations
+from disvis.helpers import mkdir_p
+
+
+def parse_args():
+    """Parse the command-line arguments."""
+
+    p = ArgumentParser(description=__doc__)
+    p.add_argument('consistent_restraints', type=int, metavar='<int>',
+        help="Minimum number of required consistent restraints.")
+    p.add_argument('-i', '--input', dest='input', type=abspath, default='.', metavar='<dir>',
+        help="Directory where the input files can be found.")
+    p.add_argument('-o', '--output', dest='output', type=abspath, default='.', metavar='<dir>',
+        help="Directory where the output file will be stored.")
+    p.add_argument('-e', '--exact', dest='exact', action='store_true',
+            help="Only write moves that are consistent with exactly the specified restraints.")
+
+    args = p.parse_args()
+    return args
+
+
+def main():
+    """Main function that will perform the function of the script."""
+
+    args = parse_args()
+
+    interspace_files = glob(join(args.input, 'red_interspace_*.mrc'))
+    nrot = len(interspace_files)
+    if nrot == 0:
+        raise ValueError("No input files where found to generate complexes.")
+
+    quats = proportional_orientations(nrot, metric='number')[0]
+    nrot_per_job = len(glob(join(args.input, 'red_interspace_0_*.mrc')))
+    fn_out = join(args.output, 
+            'consistent_moves_{:d}.txt'.format(args.consistent_restraints)
+            )
+    mkdir_p(args.output)
+
+    print 'Analyzing data ...'
+    n = 1
+    # Determine the logical operator
+    l_operator = operator.ge
+    if args.exact:
+        l_operator = operator.eq
+    with open(fn_out, 'w') as f:
+        # Write nsol x y z q0 q1 q2 q3
+        line = '{:d}' + ' {:.2f} ' * 3 + ' {:6.4f}' * 4 + '\n'
+        for fn in interspace_files:
+            job, ind = [int(x) for x in splitext(fn)[0].split('_')[2:]]
+            quat = quats[ind + job * nrot_per_job]
+            vol = Volume.fromfile(fn)
+            trans = np.asarray(l_operator(vol.array, args.consistent_restraints).nonzero()[::-1]).T * vol.voxelspacing + vol.origin
+            for t in trans:
+                data = [n] + list(t) + list(quat)
+                f.write(line.format(*data))
+                n += 1
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/generate_complexes.py
@@ -0,0 +1,79 @@
+"""Generate consistent complexes from extracted translations and rotations."""
+
+from __future__ import division
+
+from argparse import ArgumentParser
+from os.path import abspath, getsize, join, splitext
+from glob import glob
+from sys import exit
+
+import numpy as np
+
+from disvis.pdb import PDB
+from disvis.rotations import quat_to_rotmat
+from disvis.helpers import mkdir_p
+
+
+def parse_args():
+    """Parse the command-line arguments."""
+
+    p = ArgumentParser(description=__doc__)
+    p.add_argument('ligand', type=file, 
+            help="File containing the ligand structure.")
+    p.add_argument('infile', type=file,
+            help="File containing the translations and rotations.")
+    p.add_argument('-o', '--output', dest='output', type=abspath, default='.', metavar='<dir>', 
+            help="Directory where the structures will be stored.")
+    p.add_argument('-f', '--force', dest='force', action='store_true', 
+            help="Do not ask permission to write files.")
+
+    args = p.parse_args()
+    return args
+
+
+class Move(object):
+
+    def __init__(self, trans, rotmat):
+        self.trans = trans
+        self.rotmat = rotmat
+
+
+def main():
+    """Main function that will perform the function of the script."""
+
+    args = parse_args()
+
+    moves = []
+    for line in args.infile:
+        values = np.asarray(line.split()[1:], dtype=np.float64)
+        moves.append(Move(values[:3], quat_to_rotmat(values[3:].reshape(1, -1))))
+
+    # Give required memory in MBs
+    if not args.force:
+        print 'Calculating required memory ...'
+        file_size = getsize(args.ligand.name)
+        ncomplexes = len(moves)
+        required_memory = (file_size * ncomplexes / 1000000)
+        print 'This operation will require {:.0f} MB.'.format(required_memory)
+        proceed = raw_input('Proceed? [y/n]: ')
+        if proceed != 'y':
+            exit(0)
+
+    # Move ligand to the origin
+    ligand = PDB.fromfile(args.ligand)
+    ligand.translate(-ligand.coor.mean(axis=0))
+    ligand_coor = ligand.coor
+    rot_ligand = ligand.duplicate()
+
+    print 'Writing complexes to file ...'
+    mkdir_p(args.output)
+    fn = join(args.output, 'sol_{:d}.pdb')
+    for n, move in enumerate(moves, 1):
+        rot_ligand.rotate(move.rotmat)
+        rot_ligand.translate(move.trans)
+        rot_ligand.tofile(fn.format(n))
+        rot_ligand.coor = ligand_coor
+
+
+if __name__ == '__main__':
+    main()
